ee8a91984ca6183ad532314a7774b58cf0c479f7
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-01-16 10:44:59 +0530

    scope of variable in func

---
+++ variableScope.js
@@ -0,0 +1,7 @@
+var number = 10
+function multiplier(){
+    var sum = number*10
+    console.log(sum);
+}
+multiplier();
+// console.log(sum)  It does not work since the varible sum is defined locally not globally.
