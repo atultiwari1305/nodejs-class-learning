88daaeb25683f8ea3ac334235e34edacaeb36a8b
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-01-19 09:18:41 +0530

    functions

---
+++ functions1.js
@@ -0,0 +1,7 @@
+function userDetails(username, userAge){
+    return `The name is ${username} and the age is ${userAge}.`;
+}
+
+var name = 'Atul';
+var age = 20;
+console.log(userDetails(name,age));
