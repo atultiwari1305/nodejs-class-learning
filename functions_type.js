b899336e6011f35b3267277cd3fbddda59006be3
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-01-20 09:22:45 +0530

    types of function

---
+++ functions_type.js
@@ -0,0 +1,23 @@
+function sub(a,b){
+    return a-b;
+}
+console.log(sub(10,3));
+
+
+const add = function sum(a,b){
+    return a+b;
+}
+console.log(add(3,7));
+
+
+const divider = function(a,b){
+    return a/b;
+}
+console.log(divider(10,5));
+
+
+//ARROW FUNCTION
+const multiplier = (a,b)=>{
+    return a*b;
+}
+console.log(multiplier(3,7));
