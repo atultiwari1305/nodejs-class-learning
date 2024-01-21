7667975de5e6c571c9f9b9e9ed53f309919d8a33
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-01-21 09:23:19 +0530

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+//ARROW FUNCTION
+
+const add1 = (a,b)=>{
+    return a*b;
+}
+console.log(add1(3,7));
+
+const add = (a,b) => a+b;
+console.log(add(3,7));
+
+const incrementer = a => a+1;
+console.log(incrementer(5));
